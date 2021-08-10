bda0877de4730aaba52560ab6b8a99f07aad63a9
Rob Fletcher <robroy.fletcher@gmail.com>
2021-08-10 15:51:41 -0700

    test

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from pkg_resources import parse_version
 import re
 
 def GetVersion():
-    with open('test_package/__init__.py') as f:
+    with open('geodesic_test_package/__init__.py') as f:
         return parse_version(re.findall(r'__version__\s*=\s*\'([.\d]+-*[a-z]*[.\d]*)\'', f.read())[0])
 
 setup(
-    name='test_package',
+    name='geodesic_test_package',
     version=GetVersion(),
     license='Apache-2.0',
     description='A test for publishing from github actions',
     url='http://seerai.space',
     author='Me',
-    packages=["test_package"]
+    packages=["geodesic_test_package"]
 )
